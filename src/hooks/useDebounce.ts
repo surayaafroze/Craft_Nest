4298fcf963b15d860d7d044534b19bb4b5722b54
surayaafroze63 <surayaafroze63@gmail.com>
2026-07-28 15:49:23 +0600

    Add useDebounce custom hook for value throttling

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -5,7 +5,7 @@ export function useDebounce<T>(value: T, delay: number): T {
 
   useEffect(() => {
     const timer = setTimeout(() => setDebouncedValue(value), delay);
-    return () => clearTimeout(timer);
+    return () =>  clearTimeout(timer);
   }, [value, delay]);
 
   return debouncedValue;
